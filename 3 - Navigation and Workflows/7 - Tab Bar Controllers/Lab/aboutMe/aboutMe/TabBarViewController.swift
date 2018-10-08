0b793f7fda8df004a3cf6df6000166843b2cd041
Justin Snider <jcsnider100@gmail.com>
2018-10-08 08:50:03 -0600

    Lesson 3.7

---
+++ 3 - Navigation and Workflows/7 - Tab Bar Controllers/Lab/aboutMe/aboutMe/TabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarViewController.swift
+//  aboutMe
+//
+//  Created by Justin Snider on 10/8/18.
+//  Copyright © 2018 Justin Snider. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
