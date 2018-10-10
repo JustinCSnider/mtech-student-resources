cc052a6e80f30dfe5dc2f8328b192a09df3953db
Justin Snider <jcsnider100@gmail.com>
2018-10-10 09:02:44 -0600

    Lesson 3.8

---
+++ 3 - Navigation and Workflows/8 - ViewController life cycle/Lab/OrderOfEvents/OrderOfEvents/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OrderOfEvents
+//
+//  Created by Justin Snider on 10/10/18.
+//  Copyright © 2018 Justin Snider. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
