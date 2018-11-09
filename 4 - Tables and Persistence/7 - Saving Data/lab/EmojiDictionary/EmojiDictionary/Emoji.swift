e935168c62e0337b8e40192aabb8d5ab358a28a5
Justin Snider <jcsnider100@gmail.com>
2018-11-09 08:16:27 -0700

    Lesson 4.7

--- 4 - Tables and Persistence/7 - Saving Data/lab/EmojiDictionary/EmojiDictionary/Emoji.swift
+++ 4 - Tables and Persistence/7 - Saving Data/lab/EmojiDictionary/EmojiDictionary/Emoji.swift
@@ -1,9 +1,33 @@
 
 import Foundation
 
-struct Emoji {
+struct Emoji: Codable {
     var symbol: String
     var name: String
     var detailDescription: String
     var usage: String
+    
+    static func saveToFIle(emojis: [Emoji]) {
+        let propertyListEncoder = PropertyListEncoder()
+        let DocumentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let archiveURL = DocumentDirectory.appendingPathComponent("emojis").appendingPathExtension("plist")
+        
+        let encodedEmojis = try? propertyListEncoder.encode(emojis)
+        
+        try? encodedEmojis?.write(to: archiveURL, options: .noFileProtection)
+    }
+    
+    static func loadFromFile() -> [Emoji]? {
+        let propertyListDecoder = PropertyListDecoder()
+        let DocumentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let archiveURL = DocumentDirectory.appendingPathComponent("emojis").appendingPathExtension("plist")
+        
+        if let retrievedEmojiData = try? Data(contentsOf: archiveURL), let decodedEmojis = try? propertyListDecoder.decode(Array<Emoji>.self, from: retrievedEmojiData) {
+            return decodedEmojis
+        }
+        
+        return nil
+    }
+    
+    
 }
