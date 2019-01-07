e0d2488f5148ee7f3655d7ddd9fffe444ebcfaf8
Justin Snider <jcsnider100@gmail.com>
2019-01-07 09:52:52 -0700

    Finished Making flashy animations

--- Extra Projects/FlashyAnimations/FlashyAnimations/ViewController.swift
+++ Extra Projects/FlashyAnimations/FlashyAnimations/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     //========================================
     //MARK: - IBOutlets
@@ -24,19 +24,20 @@ class ViewController: UIViewController {
     //MARK: - IBActions
     //========================================
     
-    @IBAction func submitButtonTapped(_ sender: UIButton) {
-        if isValid(emailTextField.text ?? "") && (usernameTextField.text ?? "") != "" {
-            UIView.animate(withDuration: 1, animations: {
-                self.usernameTextField.alpha = 0.0
-                self.emailTextField.alpha = 0.0
-                self.submitButton.alpha = 0.0
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        let label = createCenteredLabel(with: self.usernameTextField.text ?? "")
+        UIView.animate(withDuration: 1, animations: {
+            self.usernameTextField.alpha = 0.0
+            self.emailTextField.alpha = 0.0
+            self.submitButton.alpha = 0.0
+        }) { (_) in
+            UIView.animate(withDuration: 2.25, animations: {
+                self.welcomeView.center.y += 150
+                self.welcomeView.frame.size.height += 75
+                self.welcomeView.transform = CGAffineTransform(scaleX: 1.1, y: 1)
             }) { (_) in
-                UIView.animate(withDuration: 3, animations: {
-                    self.welcomeView.center.y += 150
-                    self.welcomeView.frame.size.height += 75
-                    self.welcomeView.transform = CGAffineTransform(scaleX: 1.1, y: 1)
-                    var label = UILabel(frame: CGRect(x: self.welcomeView.center.x, y: self.welcomeView.center.y, width: 20, height: 20))
-                    
+                UIView.animate(withDuration: 1, animations: {
+                    label.alpha = 1.0
                 })
             }
         }
@@ -53,6 +54,62 @@ class ViewController: UIViewController {
         
         submitButton.layer.cornerRadius = 5
         submitButton.layer.masksToBounds = true
+        
+        usernameTextField.becomeFirstResponder()
+    }
+    
+    //========================================
+    //MARK: - Text Field Delegate Methods
+    //========================================
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if (usernameTextField.text ?? "").isEmpty {
+            UIView.animate(withDuration: 0.1, animations: {
+                self.usernameTextField.transform = CGAffineTransform(translationX: 10, y: 0)
+            }) { (_) in
+                UIView.animate(withDuration: 0.1, animations: {
+                    self.usernameTextField.transform = CGAffineTransform(translationX: -20, y: 0)
+                }) { (_) in
+                    UIView.animate(withDuration: 0.1, animations: {
+                        self.usernameTextField.transform = CGAffineTransform.identity
+                    })
+                }
+            }
+        }
+        
+        if !isValid(emailTextField.text ?? "") {
+            UIView.animate(withDuration: 0.1, animations: {
+                self.emailTextField.transform = CGAffineTransform(translationX: 10, y: 0)
+            }) { (_) in
+                UIView.animate(withDuration: 0.1, animations: {
+                    self.emailTextField.transform = CGAffineTransform(translationX: -20, y: 0)
+                }) { (_) in
+                    UIView.animate(withDuration: 0.1, animations: {
+                        self.emailTextField.transform = CGAffineTransform.identity
+                    })
+                }
+            }
+        }
+        
+        if isValid(emailTextField.text ?? "") && !(usernameTextField.text ?? "").isEmpty {
+            textField.resignFirstResponder()
+            submitButtonTapped(textField)
+        }
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField == usernameTextField {
+            let currentCharacterCount = textField.text?.count ?? 0
+            if (range.length + range.location > currentCharacterCount) {
+                return false
+            }
+            let newLength = currentCharacterCount + string.count - range.length
+            return newLength <= 8
+        } else {
+            return true
+        }
+        
     }
     
     //========================================
@@ -66,10 +123,24 @@ class ViewController: UIViewController {
             "z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5" +
             "]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-" +
             "9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21" +
-        "-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
+            "-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
         
         let emailTest = NSPredicate(format:"SELF MATCHES[c] %@", emailRegEx)
         return emailTest.evaluate(with: email)
+    }
+    
+    private func createCenteredLabel(with string: String) -> UILabel {
+        let label = UILabel(frame: CGRect(x: self.view.frame.minX, y: self.view.frame.minY, width: self.view.frame.width, height: self.view.frame.height))
+        label.text = string
+        label.font = label.font.withSize(50)
+        label.adjustsFontSizeToFitWidth = true
+        label.alpha = 0.0
+        label.textColor = UIColor.white
+        label.textAlignment = .center
+        
+        self.view.addSubview(label)
+        
+        return label
     }
     
 }
